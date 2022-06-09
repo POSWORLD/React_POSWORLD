4629ad0f503787cdf912eb60dd1c972e97ce89ed
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-06-09 17:12:44 +0900

    make route & make join

--- posworld/src/App.js
+++ posworld/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/home";
+import Join from "./components/Join/Join";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/Join" element={<Join></Join>}></Route>
+        <Route path="/Login" element={<Home></Home>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ posworld/src/components/Join/Join.js
@@ -0,0 +1,3 @@
+function Join() {}
+
+export default Join;
