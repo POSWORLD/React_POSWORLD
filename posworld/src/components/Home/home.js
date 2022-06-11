710de9819778df67dd9968a0f58f10800d33bfc6
qweasx10 <qweasx10@hanmail.net>
2022-06-11 17:48:21 +0900

    로그인

--- posworld/src/components/Home/home.js
+++ posworld/src/components/Home/home.js
@@ -1,35 +1,4 @@
-import { useEffect, useState } from "react";
-
-function Login() {
-  const [id,setId] = useState('')
-  const [pw,setPw] = useState('')
-  const handleInputId = (e) =>{
-    setId(e.target.value)
-  }
-  const handleInputPw = (e) =>{
-    setPw(e.target.value)
-  }
-  const onClickLogin = () =>{
-    console.log('click login')
-  }
-  
-  return (
-    <form name='loginForm'>
-      
-      <div>
-        <label htmlFor='id'>아이디 : </label>
-        <input type='text' name ='id' value={id} onChange={handleInputId}/>
-      </div>
-      <div>
-        <label htmlFor='pw'>비밀번호 : </label>
-        <input type='text' name ='pw' value={pw} onChange={handleInputPw}/>
-      </div>
-      <div>
-        <button type='button' onClick={onClickLogin}>로그인</button>
-      </div>
-      
-
-    </form>
-  );
+function Home() {
+  return <div>home</div>;
 }
-export default Login;
+export default Home;
