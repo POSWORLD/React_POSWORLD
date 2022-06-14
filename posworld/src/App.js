90c49e40241f40fa9cacef986113d91f3a055b04
hyunmin <qweasx10@hanmail.net>
2022-06-14 14:02:44 +0900

    mini room post

--- posworld/src/App.js
+++ posworld/src/App.js
@@ -1,20 +1,18 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home/Home";
-import Join from "./components/Join/Join";
-import Login from "./components/Login/Login";
-import "bootstrap/dist/css/bootstrap.css";
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/home';
+import Join from './components/Join/Join';
+import Login from './components/Login/Login';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/Join" element={<Join></Join>}></Route>
-        <Route path="/Login" element={<Login></Login>}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home></Home>}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- posworld/src/components/Home/home.js
+++ posworld/src/components/Home/home.js
@@ -1,10 +1,14 @@
-import AuthRouter from "../AuthRouter";
+import AuthRouter from '../AuthRouter';
 
+import { publicUrl } from '../../utils/utils';
 function Home() {
-  return (
-    <div>
-      <AuthRouter></AuthRouter>
-    </div>
-  );
+    return (
+        <div>
+            <h2>미니룸</h2>
+            <div>
+                <img src={publicUrl + '/resources/img/miniroom.gif'} alt="miniroom" />
+            </div>
+        </div>
+    );
 }
 export default Home;
