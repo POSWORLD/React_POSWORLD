ad091fb6750bf8b38d03baaf74430dff9cf183e0
hyunmin <qweasx10@hanmail.net>
2022-06-14 15:35:34 +0900

    layout complete

---
+++ posworld/src/styles/Layout/Content.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import MainMenu from '../Menu/MainMenu';
+
+const ContentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 75%;
+    h1 {
+        color: ${(props) => props.color || 'black'};
+        font-weight: bold;
+        font-size: 1.1rem;
+    }
+`;
+
+const Content = ({ children }) => {
+    return (
+        <ContentWrapper>
+            <h1>현민이의 미니룸</h1>
+            {children}
+            <MainMenu />
+        </ContentWrapper>
+    );
+};
+
+export default Content;
--- posworld/src/utils/utils.js
+++ posworld/src/utils/utils.js
@@ -1,1 +1,20 @@
 export const publicUrl = process.env.PUBLIC_URL;
+
+export function Audio(player, playlists) {
+    this.player = player;
+    this.playlists = playlists;
+    this.idx = 0;
+    this.title = '';
+
+    this.setCurTime = (curTime) => {
+        this.player.currentTime = curTime;
+    };
+
+    // 재생정보 설정
+    this.setCurrentSong = (idx, curTime) => {
+        this.idx = idx;
+        this.title = this.playlists[idx].dataset.title;
+        this.player.src = `${publicUrl}/resources/audio/${this.title}.mp3`;
+        this.setCurTime(curTime);
+    };
+}
