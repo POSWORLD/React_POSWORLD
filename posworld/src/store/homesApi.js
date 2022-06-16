c20da77e5d84d800febcdb3603153f22a70255a4
hyunmin <qweasx10@hanmail.net>
2022-06-16 10:37:54 +0900

    before photo update

---
+++ posworld/src/store/homesApi.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { customAxios } from '../http/CustomAxios';
+export const getHomeApi = async (id) => {
+    const response = await axios({
+        url: `http://localhost:8001/home/${id}`,
+        method: 'get',
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+    });
+    return response.data;
+};
+export const updateHomeApi = async (home) => {
+    try {
+        const response = await customAxios('/home/', 'put', home);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
--- posworld/src/store/store.js
+++ posworld/src/store/store.js
@@ -1,15 +1,16 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import users from "./users";
-import pComments from "./pComments";
-import photos from "./photos";
-
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import users from './users';
+import pComments from './pComments';
+import photos from './photos';
+import homes from './homes';
 const reducer = combineReducers({
-  users,
-  pComments,
-  photos,
+    users,
+    pComments,
+    photos,
+    homes,
 });
 export default configureStore({
-  reducer,
-  devTools: true,
-  middleware: (getDefaultMiddleWare) => [...getDefaultMiddleWare()],
+    reducer,
+    devTools: true,
+    middleware: (getDefaultMiddleWare) => [...getDefaultMiddleWare()],
 });
